3954b124daa3c8b3fe7ad83230507c35074cd2e8
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-02-27 16:10:01 -0300

    ci: Implementa o Terraform

---
+++ main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.2"
+
+  name                 = "rms-db"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "rms-db" {
+  name       = "rms-db"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "rms-db"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "rms-db_rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rms-db_rds"
+  }
+}
+
+resource "aws_db_parameter_group" "rms-db" {
+  name   = "rms-db-pg16-parameter-group"
+  family = "postgres16"
+
+  parameter {
+    name  = "rds.force_ssl"
+    value = "0"
+  }
+}
+
+resource "aws_db_instance" "rms-db" {
+  identifier             = "rms-db"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "16.1"
+  username               = "postgres"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.rms-db.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.rms-db.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
